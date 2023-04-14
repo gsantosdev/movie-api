30cc472944841ac6adb6aebdabc2fc7081143e5c
santo <santosgustavo431@gmail.com>
2023-04-14 16:48:49 -0300

    refactor: renmame package

---
+++ src/main/java/com/example/api/model/MovieEntity.java
@@ -0,0 +1,41 @@
+package com.example.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private String year;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column
+    private Long rating;
+
+}
